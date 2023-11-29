8a8b48c7d2e57025ef30201b98eca06438760b7b
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-11-29 18:59:06 +0530

    node-env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import userRoute from "./routes/userRoute.js";
 
 const app = express();
 
-const isDev = app.settings.env === "development";
+const isDev = process.env.NODE_ENV === "development";
 const URL = isDev ? "http://localhost:3000" : process.env.PAGE_PICKER_CLIENT;
 
 app.use(cors({ origin: URL }));
